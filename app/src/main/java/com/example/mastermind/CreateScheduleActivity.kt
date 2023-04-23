ee2729ad531c3975a5bca54ee4932bb5e6defd1a
dhha <doanhuuha@gmail.com>
2023-04-23 15:51:31 -0500

    Schedule task

--- app/src/main/java/com/example/mastermind/CreateScheduleActivity.kt
+++ app/src/main/java/com/example/mastermind/CreateScheduleActivity.kt
@@ -2,10 +2,15 @@ package com.example.mastermind
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mastermind.databinding.ActivityCreateScheduleBinding
 
 class CreateScheduleActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCreateScheduleBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_schedule)
+        binding = ActivityCreateScheduleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        title = "Create new schedule"
     }
 }
--- app/src/main/java/com/example/mastermind/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mastermind/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.mastermind.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.mastermind.CreateScheduleActivity
 import com.example.mastermind.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -31,6 +33,11 @@ class HomeFragment : Fragment() {
 //        homeViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
 //        }
+
+        binding.activityHomeFab.setOnClickListener {
+            val intent = Intent(requireContext(), CreateScheduleActivity::class.java)
+            startActivity(intent)
+        }
 
         return root
     }
