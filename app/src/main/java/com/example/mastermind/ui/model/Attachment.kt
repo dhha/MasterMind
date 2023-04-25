e34c22bada3348c738d4545b3e46c648d5e235ec
dhha <doanhuuha@gmail.com>
2023-04-25 11:28:56 -0500

    Schedule task, database

--- app/src/main/java/com/example/mastermind/ui/model/Attachment.kt
+++ app/src/main/java/com/example/mastermind/ui/model/Attachment.kt
@@ -1,3 +1,10 @@
 package com.example.mastermind.ui.model
 
-data class Attachment(val name: String, val url: Int): java.io.Serializable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Attachment(val name: String, val url: Int): java.io.Serializable {
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
+}
